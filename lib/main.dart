ca487db997c7412367bbbe7ec235764da2ba59a7
Shreejisangani <shreejisangani1@gmail.com>
2021-10-31 12:55:12 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sampleproject/home_page.dart';
+import 'package:sampleproject/pages/home_page.dart';
+import 'package:sampleproject/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Homepage();
+    return MaterialApp(
+      // home: Homepage(),
+      // theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark
+      // ),
+      initialRoute: "/home", // home bydefault hase start ma
+      routes: {
+        //ghana page hoy ane mate and koi webiste mathi lava mate pn use thay
+        "/": (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
+    );
   }
 }
