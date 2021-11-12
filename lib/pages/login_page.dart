3bd9668b45155f3357eed13c326dbf840c8e4c75
Shreejisangani <shreejisangani1@gmail.com>
2021-11-12 12:11:18 +0530

    day7

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:sampleproject/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changebutton = false;
+
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
@@ -21,7 +27,7 @@ class LoginPage extends StatelessWidget {
                   height:
                       20), //perfect scale apva mate koi vastu n atyre space apva mate
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               SizedBox(
@@ -30,31 +36,69 @@ class LoginPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.symmetric(
                     vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter username", labelText: "UserName"),
+                child: Column(children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter username", labelText: "UserName"),
+                    onChanged: (value) {
+                      name = value;
+                      setState(
+                          () {}); //stateful ma farithi call karva mate build method n
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+
+                  InkWell(
+                    onTap: () async //await mate use
+                        {
+                      setState(() {
+                        changebutton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1)); //1 second no wait karse animation mate 1 sec 6 atle pa6i home page uper jatu rese
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changebutton ? 50 : 150.0,
+                      height: 50.0,
+                      alignment: Alignment.center,
+                      child: changebutton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          // shape: changebutton? BoxShape.circle : BoxShape.rectangle,
+                          borderRadius:
+                              BorderRadius.circular(changebutton ? 50 : 8)),
                     ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter password", labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/home'); //we can also use Myroute.homepage method using MyRoute class
-                      },
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(
-                          minimumSize: Size(150,
-                              40)), //android ma ani jarur nathi but ios mate 6 style alag thai jay ana mate
-                    )
-                  ],
-                ),
+                  ),
+                  // ElevatedButton(       //button mate j 6 pn animated mate container use karvanu 6
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context,
+                  //         '/home'); //we can also use Myroute.homepage method using MyRoute class
+                  //   },
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(
+                  //       minimumSize: Size(150,
+                  //           40)), //android ma ani jarur nathi but ios mate 6 style alag thai jay ana mate
+                  // )
+                ]),
               )
             ],
           ),
